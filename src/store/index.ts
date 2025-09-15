31fd45cc4b80391270964f71af881eb68da4fcd3
rhythmitaliya <ritaliya2003@gmail.com>
2025-09-15 19:05:54 +0530

    15 Sep

--- src/store/index.ts
+++ src/store/index.ts
@@ -33,3 +33,7 @@ export const persistor = persistStore(store);
 // Types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+
+
+//================================================
